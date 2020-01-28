116d693f18ef74ab96180ee81c37675ea53ebcb6
PlantAnyTree <josea.media@gmail.com>
2020-01-28 17:57:32 -0300

    v1.2.- Controles de salida y búsqueda de duplicados

--- pelisorden.sh
+++ pelisorden.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # ~/bin/pelisorden.sh
 # CREADO: 22/01/2020
-# v1.0
+# v1.2
+# Nuevo nombre de función -> mover_archivos
+# Introducidos códigos de salida
+# Función de control sale del programa en caso de error
+# Comprobación de existencia de directorio de destino
+# fdupes para detectar duplicados cuando ya exista el directorio de destino.
+#v1.0
+
+## EXIT ##
+# 0 ->  Todo correcto
+# 1 ->  Error en recopilación de datos
+# 2 ->  Directorio de destino ya existe
 
 ## CONFIGURACIÓN ##
 RUTA_CINE="/var/media/Cine"
@@ -10,27 +21,25 @@ declare -a A_ELIMINAR=( 'TuMejorTorrent.url' 'www.DIVXATOPE.com.url' 'TUmejorTor
 'Descargas torrent Gratis Serieshd,Peliculas hd - pctnew.org.URL' 'httptumejortorrent.com.url'
 'Importante leer !!!!!.txt' 'www.newpct1.com.url' 'www.newpct.com.url' 'DESCARGAS2020.url' 'TorrentRapid.url'
 'Tumejortorrent.url' 'Descargas torrent Gratis Serieshd,Peliculas hd - Descargas2020.org.website' )
-# Extensiones de archivos de video
-declare -a A_VIDEOS=( avi mpg mpeg mkv mp4 )
-# Extensiones de otros archivos a conservar
+# Extensiones de archivos a conservar
 declare -a A_EXTENSIONES=( .avi .mpg .mpeg .mkv .mp4 -poster.jpg -fanart.jpg .es.srt .en.srt .spa.srt .eng.srt .nfo .txt )
 
 CODIGO=$0
 function usage () {
-  echo "Uso: $CODIGO [-h] [-a] [-f <archivo>] [-o <origen>] [-p <nombre de la pelicula>] [-a <año de estreno>] [-e <etiquetas>] [-t <target>]"
-  echo "  -a			Crear directorios para todos los archivos de vídeo"
+  echo "Uso: $CODIGO [-h] [-t] [-v <video>] [-o <origen>] [-p <nombre de la pelicula>] [-a <año de estreno>] [-e <etiquetas>] [-g <grupo>]"
+  echo "  -a  <año>		Año del estreno de la película"
   echo "  -h			Ayuda"
-  echo "  -f  <archivo>		Crea un directorio donde se mete el video"
+  echo "  -v  <video>		Crea un directorio y mueve el video en él"
   echo "  -o  <origen>		Archivo de la película o directorio que la contiene"
   echo "  -p  <pelicula>	Nombre de la película, puede contener el año entre paréntesis"
-  echo "  -y  <year>		Año del estreno de la película"
   echo "  -e  <etiquetas>	Etiquetas con info sobre el formato y contenido de la película, entre comillas y separadas por espacios"
   echo "  -g  <grupo>		Mueve la película al directorio del grupo asignado"
+  echo "  -t                    Crear directorios para todos los archivos de vídeo"
   echo "
 Ejemplos:
-  $CODIGO -a		->  Crea directorios para todos los archivos del directorio actual.
-  $CODIGO -f <archivo>	->  Crea un directorio del mismo nombre del archivo sin extensión y guarda el video en el.
-  $CODIGO -o <directorio> -e <etiquetas> -t <dual> -> El vídeo se encuentra en un directorio con el nombre de la película y el año de estreno
+  $CODIGO -t		->  Crea directorios para todos los archivos del directorio actual.
+  $CODIGO -v <video>	->  Crea un directorio del mismo nombre del archivo sin extensión y guarda el video en el.
+  $CODIGO -o <origen> -e <etiquetas> -t <dual> -> El vídeo se encuentra en un directorio con el nombre de la película y el año de estreno
   $CODIGO -o "Ya_no_puedo_mas.mkv" -p "Ya no puedo más" -y 2015 -e "Brip 1080p" -t es
 "
 }
@@ -54,53 +63,6 @@ function archivar_todo () {
     done < <(find ./ -maxdepth 1 -type f | sort)
 }
 
-# Imprime datos de control para comprobar si va todo bien
-function control {
-  echo "Origen: $opt_origen"
-  echo "Origen (dir): $dir_origen"
-  echo "Destino: $pelicula_dir'"
-  echo "pelicula: $pelicula'"
-  exit 0
-}
-
-# Gestión de argumentos
-while getopts aho:e:f:p:y:g: option
-  do
-    case "${option}"
-    in
-	h) usage
-   	exit 0;;
-	f) archivo "$OPTARG"
-   	exit 0;;
-	y) opt_anyo=$OPTARG;;
-	o) opt_origen="${OPTARG%/*}";;
-	e) opt_etiquetas=$(printf '[%s]' ${OPTARG[*]});;
-	p) opt_pelicula="${OPTARG%/*}";;
-	a) archivar_todo
-	  exit 0;;
-	g) opt_grupo="[$OPTARG]";;
-       \?) echo "ERROR: argumento no aceptado en este programa"
-	  usage
-	  exit 1;;
-    esac
-  done
-
-# Si la fuente es un video y no un directorio:
-[ -f "$opt_origen" ] && archivo "$opt_origen" || dir_origen="$opt_origen"
-
-# Si no paso el argumento '-p <pelicula>', la película tiene el nombre del directorio '$dir_origen'
-# Si he pasado el año como argumento. lo añado al final del nombre de la película
-pelicula_dir="${opt_pelicula:-${dir_origen}}${opt_anyo:+ (${opt_anyo})}"
-# Nueva variable con etiquetas y grupo
-# No puedo generar directamente el nombre del archivo de video por culpa del espacio entre el nombre del directorio y las etiquetas
-tags="${opt_etiquetas:+$opt_etiquetas}${opt_grupo:+$opt_grupo}"
-# Creo el nombre del archivo de video con el nombre del directorio más las etiquetas y grupo
-pelicula="$pelicula_dir${tags:+ $tags}"
-# Si existe la etiqueta 'target' añado el dato a las etiquetas y a la ruta de destino
-[ -n "$opt_grupo" ] && pelicula_dir="$RUTA_CINE/$opt_grupo/$pelicula_dir"
-
-mkdir -v "$pelicula_dir"
-
 function eliminar_basura {
   for basura in "${A_ELIMINAR[@]}"
     do
@@ -112,17 +74,83 @@ function conservar_subdirectorios {
   find "$dir_origen/"  -mindepth 1 -maxdepth 1 -type d -exec mv -b -t "$pelicula_dir"/ {} \;
 }
 
-function conservar_extensiones {
+function mover_archivos {
   for extension in "${A_EXTENSIONES[@]}"
     do
       find "$dir_origen/" -maxdepth 1 -type f -iname "*$extension" -exec mv -b --backup=numbered {} "$pelicula_dir/$pelicula$extension" \;
     done
 }
 
-#control
+function buscar_dupes {
+    read -p "¿Buscar archivos duplicados? Si/no: " read_dupes
+    dupes=${read_dupes:=si}
+    dupes=${read_dupes,,}
+    [[ "$dupes" == "si" ]] && fdupes -rd "$dir_origen" "$pelicula_dir"
+}
+
+# Verifica datos del programa. Si no los encuentra envía un error y para el programa.
+function control {
+  echo ${opt_origen?No establecida perlícula a ordenar}
+  echo ${opt_pelicula?No establecido el nombre de la película}
+  echo ${dir_origen?No establecido directorio de origen}
+  echo ${pelicula_dir?No establecido directorio de destino}
+  echo ${pelicula?No establecido nombre de destino del video}
+}
+
+# Gestión de argumentos
+while getopts tho:e:v:p:a:g: option
+  do
+    case "${option}"
+    in
+	h) usage
+   	exit 0;;
+	v) archivo "$OPTARG"
+   	exit 0;;
+	a) opt_anyo=$OPTARG;;
+	o) opt_origen="${OPTARG%/*}";;
+	e) opt_etiquetas=$(printf '[%s]' ${OPTARG[*]});;
+	p) opt_pelicula="${OPTARG%/*}";;
+	t) archivar_todo
+	  exit 0;;
+	g) opt_grupo="[$OPTARG]";;
+       \?) echo "ERROR: argumento no aceptado en este programa"
+	  usage
+	  exit 1;;
+    esac
+  done
+
+
+# Si la fuente es un video y no un directorio:
+[ -f "$opt_origen" ] && archivo "$opt_origen" || dir_origen="$opt_origen"
+
+# Si no paso el argumento '-p <pelicula>', la película tiene el nombre del directorio '$dir_origen'
+# Si he pasado el año como argumento. lo añado al final del nombre de la película
+pelicula_dir="${opt_pelicula:-${dir_origen}}${opt_anyo:+ (${opt_anyo})}"
+# Nueva variable con etiquetas y grupo
+# No puedo generar directamente el nombre del archivo de video por culpa del espacio entre el nombre del directorio y las etiquetas
+etiquetas="${opt_etiquetas:+$opt_etiquetas}${opt_grupo:+$opt_grupo}"
+# Creo el nombre del archivo de video con el nombre del directorio más las etiquetas y grupo
+pelicula="$pelicula_dir${etiquetas:+ $etiquetas}"
+# Si existe la etiqueta 'target' añado el dato a las etiquetas y a la ruta de destino
+[ -n "$opt_grupo" ] && pelicula_dir="$RUTA_CINE/$opt_grupo/$pelicula_dir"
+
+control
+
+if [[ ! -d "$pelicula_dir" ]]
+  then
+    mkdir -v "$pelicula_dir"
+  else
+    echo "El directorio de destino ya existe: $pelicula_dir"
+    ls -ahl "$pelicula_dir"
+    buscar_dupes
+    read -p "¿Continuar moviendo el archivo $pelicula en este directorio? Si/no: " read_continuar
+    continuar=${read_continuar,,}
+    [[ "$continuar" == "no" ]] && exit 2
+  fi
+
 eliminar_basura
 conservar_subdirectorios
-conservar_extensiones
+mover_archivos
 
 rmdir -v "$dir_origen"
 
